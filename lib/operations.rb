378bec90a6ecd24164cba0a6d27bb81a7935a615
Robyn Taylor <rhtaylor0715@gmail.com>
2021-03-26 21:10:57 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-
+  if speed > 60 || speed < 40
+    return true
+  else speed.between?(40, 60)
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+  return > 60 || speed < 40 ? "True" : "False"
 end
-	
-
-
