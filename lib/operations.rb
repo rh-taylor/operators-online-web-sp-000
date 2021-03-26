c81af33d23339fc41cbe0107df621ca07c36f6d7
Robyn Taylor <rhtaylor0715@gmail.com>
2021-03-26 21:13:12 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-
+  if speed > 60 || speed < 40
+    return true
+  else speed.between?(40, 60)
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+  return speed > 60 || speed < 40 ? true : false
 end
-	
-
-
